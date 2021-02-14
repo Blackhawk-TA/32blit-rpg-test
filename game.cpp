6202c997fd01a4278d24a7351089aa15e4312177
Blackhawk <daniel.peters1@pm.me>
2021-02-14 17:27:22 +0100

    Load first layer of tilemap

--- game.cpp
+++ game.cpp
@@ -1,7 +1,35 @@
+#include <string>
+#include <string.h>
+#include <memory>
+#include <cstdlib>
 #include "game.hpp"
 
 using namespace blit;
 
+constexpr uint16_t screen_width = 160;
+constexpr uint16_t screen_height = 120;
+
+constexpr uint16_t level_width = 64;
+constexpr uint16_t level_height = 64;
+
+uint8_t *level_data;
+TileMap* level;
+
+#pragma pack(push,1)
+struct TMX {
+	char head[4];
+	uint8_t empty_tile;
+	uint16_t width;
+	uint16_t height;
+	uint16_t layers;
+	uint8_t data[];
+};
+#pragma pack(pop)
+
+Mat3 camera;
+std::function<Mat3(uint8_t)> level_line_interrupt_callback = [](uint8_t y) -> Mat3 {
+	return camera;
+};
 ///////////////////////////////////////////////////////////////////////////
 //
 // init()
@@ -9,35 +37,50 @@ using namespace blit;
 // setup your game here
 //
 void init() {
-    set_screen_mode(ScreenMode::hires);
+	set_screen_mode(ScreenMode::hires);
+	screen.sprites = SpriteSheet::load(asset_objects);
+
+	level_data = (uint8_t *)malloc(level_width * level_height);
+	level = new TileMap((uint8_t *)level_data, nullptr, Size(level_width, level_height), screen.sprites);
+
+	TMX *tmx = (TMX *)asset_map;
+
+	if(tmx->width > level_width) return;
+	if(tmx->height > level_height) return;
+
+	// Load the level data from the map memory
+	memset(level_data, 0, level_width * level_height);
+
+	for(auto x = 0u; x < tmx->width; x++) {
+		for(auto y = 0u; y < tmx->height; y++) {
+			auto src = y * tmx->width + x;
+			auto dst = y * level_width + x;
+			level_data[dst] = tmx->data[src];
+		}
+	}
+
 }
 
 ///////////////////////////////////////////////////////////////////////////
 //
 // render(time)
 //
-// This function is called to perform rendering of the game. time is the 
+// This function is called to perform rendering of the game. time is the
 // amount if milliseconds elapsed since the start of your game
 //
 void render(uint32_t time) {
 
-    // clear the screen -- screen is a reference to the frame buffer and can be used to draw all things with the 32blit
-    screen.clear();
+	// clear the screen -- screen is a reference to the frame buffer and can be used to draw all things with the 32blit
+	screen.clear();
 
-    // draw some text at the top of the screen
-    screen.alpha = 255;
-    screen.mask = nullptr;
-    screen.pen = Pen(255, 255, 255);
-    screen.rectangle(Rect(0, 0, 320, 14));
-    screen.pen = Pen(0, 0, 0);
-    screen.text("Hello 32blit!", minimal_font, Point(5, 4));
+	level->draw(&screen, Rect(0, 0, screen.bounds.w, screen.bounds.h), level_line_interrupt_callback);
 }
 
 ///////////////////////////////////////////////////////////////////////////
 //
 // update(time)
 //
-// This is called to update your game state. time is the 
+// This is called to update your game state. time is the
 // amount if milliseconds elapsed since the start of your game
 //
 void update(uint32_t time) {

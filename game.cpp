86846aaf146ba604d1a8051463fb1fe76096ac09
Blackhawk <daniel.peters1@pm.me>
2021-02-20 19:58:22 +0100

    Implement multi-layer rendering

--- game.cpp
+++ game.cpp
@@ -10,9 +10,10 @@ constexpr uint16_t screen_height = 120;
 
 constexpr uint16_t level_width = 64;
 constexpr uint16_t level_height = 64;
+constexpr uint32_t level_size = level_width * level_height;
 
-uint8_t *level_data;
-TileMap* level;
+uint8_t *layer_data[4];
+TileMap *layers[4];
 
 #pragma pack(push,1)
 struct TMX {
@@ -39,22 +40,24 @@ void init() {
 	set_screen_mode(ScreenMode::hires); //lores is zoomed
 	screen.sprites = SpriteSheet::load(asset_objects);
 
-	level_data = (uint8_t *)malloc(level_width * level_height);
-	level = new TileMap((uint8_t *)level_data, nullptr, Size(level_width, level_height), screen.sprites);
-
 	TMX *tmx = (TMX *)asset_map;
 
 	if(tmx->width > level_width) return;
 	if(tmx->height > level_height) return;
 
-	// Load the level data from the map memory
-	memset(level_data, 0, level_width * level_height);
+	for(auto i = 0u; i < tmx->layers; i++) {
+		layer_data[i] = (uint8_t *)malloc(level_size * tmx->layers);
+		layers[i] = new TileMap((uint8_t *)layer_data[i], nullptr, Size(level_width, level_height), screen.sprites);
 
-	for(auto x = 0u; x < tmx->width; x++) {
-		for(auto y = 0u; y < tmx->height; y++) {
-			auto src = y * tmx->width + x;
-			auto dst = y * level_width + x;
-			level_data[dst] = tmx->data[src];
+		// Load the level data from the map memory
+		memset(layer_data[i], 0, level_size * tmx->layers);
+
+		for (auto x = 0u; x < tmx->width; x++) {
+			for (auto y = 0u; y < tmx->height; y++) {
+				auto src = y * tmx->width + x;
+				auto dst = y * level_width + x;
+				layer_data[i][dst] = tmx->data[src + i * level_size];
+			}
 		}
 	}
 }
@@ -70,7 +73,9 @@ void render(uint32_t time) {
 	// clear the screen -- screen is a reference to the frame buffer and can be used to draw all things with the 32blit
 	screen.clear();
 
-	level->draw(&screen, Rect(0, 0, screen.bounds.w, screen.bounds.h), level_line_interrupt_callback);
+	for (auto & layer : layers) {
+		layer->draw(&screen, Rect(0, 0, screen.bounds.w, screen.bounds.h), level_line_interrupt_callback);
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////

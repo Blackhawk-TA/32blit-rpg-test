ab3f3449d95e0c94cdc072f31e0afdc08a0f0ddc
Blackhawk <daniel.peters1@pm.me>
2021-03-21 00:16:42 +0100

    Use pointer for get_flag method

--- src/utils/layer_handler.hpp
+++ src/utils/layer_handler.hpp
@@ -21,7 +21,7 @@ public:
 	static void draw_map(std::function<Mat3(uint8_t)> *level_line_interrupt_callback);
 	static void generate_map();
 	static void set_flags(LayerHandler::TileFlags flag, const std::vector<uint8_t> &tiles);
-	static uint8_t get_flag(Point p);
+	static uint8_t get_flag(Point &p);
 };
 
 #endif //RPG_TEST_LAYER_HANDLER_HPP
